19bd102fd78537c3a2a32ae0f0fe6edb02f69c78
JIPHIA <arthur.pub1@gmail.com>
2025-02-05 16:44:28 +0100

    text

---
+++ index.js
@@ -0,0 +1,6 @@
+alert("j'ai pas réussi le alerte qaund on click")
+
+btn1.addEventListener("click", () => {
+console.log("method 3 sur le bouton à l'id btn-1");
+response.classList.alert("tu a cliquer")
+})
